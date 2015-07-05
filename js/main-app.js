f33ccf3161a5798fe163a411f558dfe765da8245
Swolebrain <vic574m@gmail.com>
2015-07-05 00:56:41 -0400

    First commit
    ~

---
+++ js/main-app.js
@@ -0,0 +1,24 @@
+var map, infoWindow;
+/*
+$(document).ready(function(){
+   var mapOptions = {
+        zoom : 5,
+        center : new google.maps.LatLng(37, -100),
+        panControl : false,
+        zoomControl : true,
+        zoomControlOptions : {
+            style : google.maps.ZoomControlStyle.LARGE,
+            position : google.maps.ControlPosition.RIGHT_CENTER
+        },
+        scaleControl : false
+   };
+   
+   infoWindow = new google.maps.infoWindow({
+        content : "holding"
+   });
+   
+   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+   
+});
+
+*/
